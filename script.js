ae9a6369981d302be4a32b35dba723bf2e76e4b2
Marcel Cayuela Dolcet <149514264+mcayuela@users.noreply.github.com>
2026-02-04 20:54:11 +0100

    portafoli_v2
    Creació d'un bon inici

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var toggleNeon = document.getElementById('toggleNeon');
-    var sections = document.querySelectorAll('.contenidor section');
-    var button = document.querySelector('.boto-apagar-neon');
-    var h1 = document.querySelector('h1');
-
-    if (localStorage.getItem('neonState') === 'on') {
-        toggleNeon.checked = true;
-        sections.forEach(function(section) {
-            section.classList.add('apagar-neon');
-        });
-        button.classList.add('apagar-neon');
-        h1.classList.add('apagar-neon');
-    }
-
-    toggleNeon.addEventListener('change', function() {
-        sections.forEach(function(section) {
-            section.classList.toggle('apagar-neon');
-        });
-
-        button.classList.toggle('apagar-neon');
-        h1.classList.toggle('apagar-neon');
-
-        if (toggleNeon.checked) {
-            localStorage.setItem('neonState', 'on');
-        } else {
-            localStorage.setItem('neonState', 'off');
-        }
-    });
+document.addEventListener('mousemove', (e) => {
+    const spotlight = document.getElementById('spotlight');
+    const x = e.clientX;
+    const y = e.clientY;
+    spotlight.style.setProperty('--x', `${x}px`);
+    spotlight.style.setProperty('--y', `${y}px`);
 });
